a92a32f2b3d9bd50c3e9d6f351e239e6fb8e2085
Jorman Garcia <jagarcia@lfs-inc.com>
2024-02-14 22:40:25 -0500

    Add new project in python and abstraction exercise

--- Fundamentals_OOP/obj/Debug/net8.0/Fundamentals_OOP.AssemblyInfo.cs
+++ Fundamentals_OOP/obj/Debug/net8.0/Fundamentals_OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentals_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781f609b29a9ab258310a9bf6ddc2b2b90afb7fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentals_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentals_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
